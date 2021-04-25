facc6e04e6dee362fecd210744b900a91f308ed0
AcedYu <acedyu@DESKTOP-F4GSCBI.localdomain>
2021-04-25 13:56:56 -0700

    Strayed away from object Company

--- index.js
+++ index.js
@@ -1,8 +1,117 @@
-// Import Company
-const Company = require('./src/Company');
+// grab modules
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+// grab database
+const connection = require('./database/readData.js');
+const server = require('./database/routes.js');
 
-// Create Company
-const company = new Company;
+const start = () => {
+  console.log("Welcome to the Company Employee Management System.");
+  connection.connect((err) => {
+    if (err) throw err;
+    ask();
+  });
+}
 
-// Invoke company.start
-company.start();
+const ask = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: ['View All Employees', 'View All Roles', 'View All Departments', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Add Role', 'Add Department', 'Remove Employee', 'Remove Role', 'Remove Department', 'Update Employee Role', 'Update Employee Manager', 'EXIT']
+    })
+    .then(({ choice }) => {
+      switch (choice) {
+        case 'View All Employees':
+          viewEmployees();
+          break;
+        case 'View All Roles':
+          viewRoles();
+          break;
+        case 'View All Departments':
+          viewDepartments();
+          break;
+        case 'View All Employees By Department':
+          viewEmployeesbyDepartment();
+          break;
+        case 'View All Employees By Manager':
+          break;
+        case 'Add Employee':
+          break;
+        case 'Add Role':
+          break;
+        case 'Add Department':
+          break;
+        case 'Remove Employee':
+          break;
+        case 'Remove Role':
+          break;
+        case 'Remove Department':
+          break;
+        case 'Update Employee Role':
+          break;
+        case 'Update Employee Manager':
+          break;
+        case 'EXIT':
+          console.log('Exiting the program');
+          connection.end();
+          break;
+      }
+    })
+}
+
+const askAgain = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "Would you like to make another query?",
+      name: "choice",
+      choices: ['yes', 'no']
+    })
+    .then(({ choice }) => {
+      switch (choice) {
+        case 'yes':
+          ask();
+          break;
+        case 'no':
+          console.log('Exiting the program');
+          connection.end();
+          break;
+      }
+    });
+}
+
+const viewEmployees = () => {
+  server.viewEmployees(askAgain);
+}
+
+const viewRoles = () => {
+  server.viewRoles(askAgain);
+}
+
+const viewDepartments = () => {
+  server.viewDepartments(askAgain);
+}
+
+const viewEmployeesbyDepartment = () => {
+  server.getDepartments((data) => {
+    var departments = [];
+    for (let department of data) {
+      departments.push(department.name);
+    }
+    inquirer
+      .prompt({
+        type: "list",
+        message: "Which department would you like to view?",
+        name: "department",
+        choices: departments
+      })
+      .then(({department}) => {
+        var id = departments.indexOf(department) + 1;
+        server.viewEmployeesbyDepartment(id, askAgain);
+      })
+  });
+}
+
+start();
